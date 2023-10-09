b760a82e90c1474c67f8367759ea369ecfb71d9d
hfidelis <heitorc88@gmail.com>
2023-10-09 00:11:06 -0300

    feat: login page added (TODO)

---
+++ src/app/components/pages/login-page/login-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+
+}
