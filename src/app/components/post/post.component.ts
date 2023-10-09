492421e7d7971f9b02a23a04751ded169cae7a35
hfidelis <heitorc88@gmail.com>
2023-10-09 00:09:38 -0300

    refactor: use new props

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -2,9 +2,6 @@ import { Component, Input } from '@angular/core';
 import { Post } from 'src/app/models/Post.model';
 import { IconDefinition, faEye, faCalendarDays, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
-import { PostService } from '../../services/post.service';
-import { User } from 'src/app/models/User.model';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post',
@@ -16,15 +13,12 @@ export class PostComponent {
   faEye: IconDefinition = faEye;
   faCalendarDays: IconDefinition = faCalendarDays;
   faUser: IconDefinition = faUser;
-  user$?: Observable<User>;
 
   constructor(
     private router: Router,
-    private postService: PostService
   ) { }
 
   ngOnInit(): void {
-    this.user$ = this.postService.getUserById(this.post.creatorId)
   }
 
   pageRedirect(id: number): void {
