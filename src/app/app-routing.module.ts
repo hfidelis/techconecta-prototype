8406950e22ee1bbc5a45a53ec968671421a0aaaa
Thiago Lins <thiago.jorge2001@gmail.com>
2023-10-17 22:40:42 -0300

    Tela de criação dos posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { PostPageComponent } from './components/pages/post-page/post-page.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'posts/:id', component: PostPageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent }
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'create-post', component: CreatePostComponent}
 ];
 
 @NgModule({
